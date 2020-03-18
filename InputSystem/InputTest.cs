eb912e2dd83c9c560f2d327b4860ef2f9bc1647c
RexYuLei <375923360@qq.com>
2020-03-18 19:10:07 +0800

    添加输入系统

---
+++ InputSystem/InputTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    private void Awake()
+    {
+        Input.GetKeyDown(KeyCode.A);
+        Vector3 pos = Input.mousePosition;
+        
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log("按键");
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("预定义按键名字");
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("鼠标按键按下,0是左键,1是右键,2是中间滚轮");
+        }
+
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("任何键按下");
+        }
+
+        if (Input.inputString != "")
+        {
+            Debug.Log(Input.inputString);    
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+    }
+}
+
+
